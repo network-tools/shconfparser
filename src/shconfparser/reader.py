66ca5cd46a3e7903807b17568eb83f37deca3e24
Kiran Kumar Kotari <kirankotari@live.com>
2018-12-29 22:29:52 +0530

    adding data reader

---
+++ src/shconfparser/reader.py
@@ -0,0 +1,16 @@
+from os import path
+from io import open
+
+class Reader:
+    def __init__(self, path):
+        self.path = path
+        self.data = self.read()
+
+    def _isfile(self):
+        if path.exists(self.path):
+            return os.path.isfile(self.path)
+
+    def read(self):
+        if self._isfile():
+            with open(self.path) as f:
+                return f.readlines()
