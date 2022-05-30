4b1dd489fec7420e39b6f04fef8ef681a828ec65
Kiran Kumar Kotari <kirankotari@live.com>
2022-05-30 05:00:49 -0400

    Update __init__.py

--- shconfparser/__init__.py
+++ shconfparser/__init__.py
@@ -1,6 +1,6 @@
 import logging
 from datetime import datetime
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 # logging.basicConfig(filename='logs/shconfparser.log', level=logging.DEBUG)
 logging.info('Logging Time : {}'.format(datetime.now()))
